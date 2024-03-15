01713f3e02ef18b11110de0a4665f6d59685015e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-15 12:21:07 +0100

    12 Blog Remote MDX: starting: pulling from course 6

---
+++ 12_Blog_with_Remote_MDX_Content_Files/components/Posts.tsx
@@ -0,0 +1,17 @@
+import { getSortedAllPostsFrontMatter } from "@/libs";
+import ListItem from "./ListItem";
+
+export default function Posts() {
+  const posts = getSortedAllPostsFrontMatter();
+
+  return (
+    <section className="mt-6 mx-auto max-w-2xl">
+      <h2 className="text-4xl font-bold dark:text-white/90">Blogs List</h2>
+      <ul className="w-full">
+        {posts.map((post) => (
+          <ListItem key={post.id} post={post} />
+        ))}
+      </ul>
+    </section>
+  );
+}
---
+++ 12_Blog_with_Remote_MDX_Content_Files/libs/index.ts
@@ -0,0 +1,5 @@
+import { getSortedAllPostsFrontMatter } from "./getSortedAllPostsFrontMatter";
+import { getFormattedDate } from "./getFormattedDate";
+import { getSinglePostData } from "./getSinglePostData";
+
+export { getFormattedDate, getSortedAllPostsFrontMatter, getSinglePostData };

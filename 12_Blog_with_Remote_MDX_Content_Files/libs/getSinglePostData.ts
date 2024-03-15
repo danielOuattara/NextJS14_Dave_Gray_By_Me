1ebf56d3b436709181683d0bc3e37bcee6f252ba
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-15 15:02:49 +0100

    12 Blog Remote MDX: getPostsMeta( )

--- 12_Blog_with_Remote_MDX_Content_Files/libs/getSinglePostData.ts
+++ 12_Blog_with_Remote_MDX_Content_Files/libs/getSinglePostData.ts
@@ -1,1 +1,39 @@
-export async function getPostsMeta(id: string) {}
+import { Metadata } from "next";
+
+type FileTree = {
+  tree: [{ path: string }];
+};
+
+export async function getPostsMeta(
+  id: string,
+): Promise<TypeMeta[] | undefined> {
+  const res = await fetch(
+    "https://github.com/danielOuattara/Mdx_Posts_Source/tree/main?recursive=1",
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    },
+  );
+  if (!res.ok) {
+    console.log(res.statusText);
+    return undefined;
+  }
+
+  const repoFileTree: FileTree = await res.json();
+  const filesArray = repoFileTree.tree
+    .map((obj) => obj.path)
+    .filter((path) => path.endsWith(".mdx"));
+
+  const posts: TypeMeta[] = [];
+
+  for (const file of filesArray) {
+    const post = await getPostByName(file);
+    if (post) {
+      posts.push(post.meta);
+    }
+  }
+  return posts.sort((a, b) => (a.date < b.date ? 1 : -1));
+}

40a6b052bba7ea8586344954da71ef9f9bf5a119
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-03 09:29:49 +0200

    12 Blog Remote MDX:  getSortedAllPostMeta( )

--- 12_Blog_with_Remote_MDX_Content_Files/libs/getSortedAllPostsMeta.ts
+++ 12_Blog_with_Remote_MDX_Content_Files/libs/getSortedAllPostsMeta.ts
@@ -1,63 +1,8 @@
-import { Metadata } from "next";
-import { compileMDX } from "next-mdx-remote/rsc";
+import { getPostByName } from "./index";
 
-type FileTree = {
-  tree: [{ path: string }];
-};
-
-export async function getPostByName(
-  fileName: string,
-): Promise<TypeBlogPost | undefined> {
-  const res = await fetch(
-    `${process.env.GITHUB_FETCH_SINGLE_URL}/${fileName}`,
-    {
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    },
-  );
-
-  if (!res.ok) {
-    console.log(res.statusText);
-    return undefined;
-  }
-
-  const rawMDX = await res.text();
-
-  if (rawMDX === "404: Not Found") {
-    return undefined;
-  }
-
-  const { frontmatter, content } = await compileMDX<{
-    title: string;
-    date: string;
-    tags: string[];
-  }>({
-    source: rawMDX,
-    options: {
-      parseFrontmatter: true,
-    },
-  });
-
-  const id = fileName.replace(/\.mdx$/, "");
-
-  const blogPostObj: TypeBlogPost = {
-    meta: {
-      id,
-      title: frontmatter.title,
-      date: frontmatter.date,
-      tags: frontmatter.tags,
-    },
-    content,
-  };
-  return blogPostObj;
-}
-
-//---------------------------------------------------------------------------------
-
-export async function getSortedAllPostsMeta(): Promise<TypeMeta[] | undefined> {
+export default async function getSortedAllPostsMeta(): Promise<
+  TypeMeta[] | undefined
+> {
   const res = await fetch(process.env.GITHUB_FETCH_URL as string, {
     headers: {
       Accept: "application/vnd.github+json",
@@ -73,21 +18,21 @@ export async function getSortedAllPostsMeta(): Promise<TypeMeta[] | undefined> {
   const repoFileTree: FileTree = await res.json();
   // console.log("repoFileTree = ", repoFileTree);
 
-  const filesArray = repoFileTree.tree
+  const filesNameArray = repoFileTree.tree
     .map((obj) => obj.path)
     .filter((path) => path.endsWith(".mdx"));
 
-  console.log("filesArray = ", filesArray);
+  // console.log("filesNameArray = ", filesNameArray);
 
   const posts: TypeMeta[] = [];
 
-  for (const file of filesArray) {
-    const post = await getPostByName(file);
+  for (const fileName of filesNameArray) {
+    const post = await getPostByName(fileName);
     if (post) {
       posts.push(post.meta);
     }
   }
 
-  console.log("posts = ", posts);
+  // console.log("posts = ", posts);
   return posts.sort((a, b) => (a.date < b.date ? 1 : -1));
 }
